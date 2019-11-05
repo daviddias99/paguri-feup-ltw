a2b487fa5ef8f9a7a0cb81b9499cadc4da9d71b7
luispcunha <luispplmcunha@gmail.com>
2019-11-05 19:02:14 +0000

    Fix merge conflicts

--- templates/common/footer.php
+++ templates/common/footer.php
@@ -1,10 +1,6 @@
 <?php function draw_footer() { ?>
             <footer>
-<<<<<<< HEAD
                 <p>&copy; Paguri WIP</p>
-=======
-                <p>&copy; Footer a completar</p>
->>>>>>> ed5a50742eebb8130524e1de6b18badfe8af46ad
             </footer>
         </body>
     </html>
