cb93e1499affec4fc085d8e84d01ae0d5967e21b
GambuzX <mg_mesquita@hotmail.com>
2019-11-07 17:51:36 +0000

    User related queries

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 
     include_once('templates/common/header.php');
     include_once('templates/common/footer.php');
+    include_once('database/user_queries.php');
 
     display_header();
+
+    createUser('insertTest4', 'test4@gmail.com', 'sal grosso', '1234');
+    print_r(getAllUsers());
+
     display_footer();
 
 ?>
