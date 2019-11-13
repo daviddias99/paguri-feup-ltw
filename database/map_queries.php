1d6ef5653881e34f2a4d546c29a6ee65eedcf18f
GambuzX <mg_mesquita@hotmail.com>
2019-11-13 10:35:42 +0000

    More queries; Updated UML

--- database/map_queries.php
+++ database/map_queries.php
@@ -14,5 +14,18 @@
         return $stmt->fetchAll();
     }
 
+    function getResidenceMapInfo($residenceID) {
+        global $dbh;
+
+        $stmt = $dbh->prepare(
+            'SELECT address, city, country, latitude, longitude, residencetype.name as type
+            FROM residence JOIN residencetype
+            ON residence.type = residenceTypeID
+            WHERE residenceID = ?'
+        );
+        $stmt->execute(array($residenceID));
+        return $stmt->fetch();
+    }
+
 
 ?>
--- index.php
+++ index.php
@@ -4,20 +4,9 @@
     include_once('templates/common/footer.php');
     include_once('database/user_queries.php');
     include_once('database/residence_queries.php');
+    include_once('database/map_queries.php');
 
-    display_header();
 
-    /*$comments = getResidenceComments(1);
-    print_r($comments);
-
-    foreach($comments as $comment) {
-
-        $replies = getCommentReplies($comment['commentID']);
-        print_r($replies);
-
-    }*/
-    //print_r(getResidenceInfo(3));
-
-    display_footer();
+    print_r(getResidenceMapInfo(1));
 
 ?>
