3fe9ad7a4fe87aecf6fab139268ea1a4147d0dd7
David Silva <up201705373@fe.up.pt>
2019-11-20 10:27:13 +0000

    Started working on search results

--- index.php
+++ index.php
@@ -2,22 +2,10 @@
 
     include_once('templates/common/header.php');
     include_once('templates/common/footer.php');
-    include_once('database/user_queries.php');
-    include_once('database/residence_queries.php');
+    include_once('templates/front_page_elements.php');
 
-    display_header();
-
-    /*$comments = getResidenceComments(1);
-    print_r($comments);
-
-    foreach($comments as $comment) {
-
-        $replies = getCommentReplies($comment['commentID']);
-        print_r($replies);
-
-    }*/
-    //print_r(getResidenceInfo(3));
-
-    display_footer();
+    draw_header('front_page');
+    draw_search_box();
+    draw_footer();
 
 ?>
