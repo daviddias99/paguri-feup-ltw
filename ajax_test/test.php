8872e97b10974f210c08f532319e6b791a8f89a9
GambuzX <mg_mesquita@hotmail.com>
2019-11-24 18:25:53 +0000

    Creating a map; Markers and markers clusters

--- ajax_test/test.php
+++ ajax_test/test.php
@@ -3,8 +3,11 @@
     <head>
         <title>Free houses</title>    
         <meta charset="UTF-8">
-        <link href="../css/style.css" rel="stylesheet">
+        <link href="map.css" rel="stylesheet">
         <script src="geocoding_ajax.js" defer></script>
+        <script src="google_map.js" defer></script>
+        <script src="./map_clusters/markerclusterer.js" defer></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyD0_ruPYuzA1ntPaautYqVqtjOT96oNLSE&callback=initMap" async defer></script>
     </head>
     <body>
         <header>
@@ -16,5 +19,7 @@
             <input id="latitude_bar" type="text" placeholder="latitude" disabled>
             <input id="longitude_bar" type="text" placeholder="longitude" disabled>
         </form>
+
+        <div id="map"></div>
     </body>
 </html>
