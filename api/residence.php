d620292a6d2c965924f0d49e277777f6b1984373
GambuzX <mg_mesquita@hotmail.com>
2019-12-05 21:52:31 +0000

    Get method for residences

--- api/residence.php
+++ api/residence.php
@@ -1,25 +1,41 @@
 <?php
 
+    include_once('./response_status.php');
+
     $request_method = $_SERVER['REQUEST_METHOD'];
     $accept_header = $_SERVER['HTTP_ACCEPT'];
 
+    $response = array('status' => ResponseStatus::OK);
+
     if ($accept_header != 'application/json') {
-        echo 'Only JSON is supported.';
+        $response['status'] = ResponseStatus::BAD_REQUEST;
+        echo json_encode($response);
         die();
-        // send error in api or html code 500
     }
 
     include_once("../database/residence_queries.php");
 
     if($request_method == 'GET') {
-        $info = getAllResidences();
+        
+        if(array_key_exists('id', $_GET)) {
+            $info = getResidenceInfo($_GET['id']);
+            
+            if ($info == FALSE) 
+                $response['status'] = ResponseStatus::NOT_FOUND;
+            else
+                $response['residence'] = $info;
+        }
+        else {
+            $response['residences'] = getAllResidences();
+        }
 
-        echo json_encode($info);
-        //echo json_encode(array('residences' => $info));
+        echo json_encode($response);
     }
 
     else if ($request_method == 'POST') {
         echo 'post';
     }
+
+
 
 ?>
---
+++ api/response_status.php
@@ -0,0 +1,13 @@
+<?php
+
+    abstract class ResponseStatus {
+        const OK = 200;
+        const CREATED = 201;
+        const BAD_REQUEST = 400;
+        const FORBIDDEN = 403;
+        const NOT_FOUND = 404;
+        const METHOD_NOT_ALLOWED = 405;
+        const IM_A_TEAPOT = 418;
+        const INTERNAL_SERVER_ERROR = 500;
+    }
+?>
