95778b52fe4eceb67912b821277ecfd5db423781
GambuzX <mg_mesquita@hotmail.com>
2019-12-13 16:48:13 +0000

    Added sessions bak to add_house page

--- pages/add_house.php
+++ pages/add_house.php
@@ -5,14 +5,13 @@
     include_once('../templates/map/includes.php');
     include_once('../templates/houses.php');
 
-    //if (! isset($_SESSION['username']))
-    //    die("User not logged in!");
+    if (! isset($_SESSION['username']))
+        die("User not logged in!");
 
-    //$username = $_SESSION['username'];
+    $username = $_SESSION['username'];
 
-    //draw_header('search_results', $username);
+    draw_header('search_results', $username);
 
-    draw_header('search_results', 'username');
     add_map_includes();
 
     draw_add_house();
